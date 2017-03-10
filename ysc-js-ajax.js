4f7c8d8bf184e1c1140bac924212a29038a4fb0c
杨松潮 <879668219@qq.com>
2017-03-10 08:52:20 +0800

    Update ysc-js-ajax.js

--- ysc-js-ajax.js
+++ ysc-js-ajax.js
@@ -7,7 +7,7 @@
 //{boolean}opt.async （可选，ture/false,默认值true)是否为异步请求，true为异步的，false为同步的
 //{object}opt.data （post方法时postData和data至少有一个）发送的参数，格式为对象类型
 //{function}opt.success ajax 发送并接收成功调用的回调函数
-//{function}opt.failure (可选）ajax发送并接收成功调用的回调函数
+//{function}opt.error (可选）ajax发送并接收成功调用的回调函数
 
 //17.3.9: !注意我这个封装函数中的方法是用opt.method而不是opt.type来表示
 
@@ -58,8 +58,8 @@ function ajax(opt) {
                     opt.success(d);
                 }
             } else {
-                if (typeof opt.fail == 'function') {
-                    opt.failure();
+                if (typeof opt.error == 'function') {
+                    opt.error();
                 }
             }
         }
